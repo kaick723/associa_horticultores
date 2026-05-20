908e56bfe0e405095dc2bb51b77a299e7bbd2409
Kaick Vasconcelos <86269939+kaick723@users.noreply.github.com>
2026-05-20 15:29:49 -0300

    Update main.jsx

--- market place/frontend/src/main.jsx
+++ market place/frontend/src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Root from './Root'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Root />
-  </StrictMode>,
-)
+import App from "./App";
+import ProductDetails from "./pages/ProductDetails";
+
+function Root() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<App />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Root;
